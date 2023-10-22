f6570fc7afeefd8ff6bf5b068d3835e425a3332c
SuhaiBatman <97989770+SuhaiBatman@users.noreply.github.com>
2023-10-22 01:42:04 -0500

    created two files

--- backend/bruhbruh.py
+++ backend/bruhbruh.py
@@ -1,19 +1,5 @@
-import os
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
 import cv2
 import mediapipe as mp
-from flask import Flask
-
-app = Flask(__name__)
-
-mongo_client = MongoClient(os.getenv("MONGO_CONNECTION"), server_api=ServerApi('1'))
-db = mongo_client['LLM']
-users_collection = db['Values']
-
-@app.route("/")
-def insert():
-    users_collection.insert_one("bruh")
 
 def main():
     # Initialize MediaPipe Hand module
@@ -80,4 +66,4 @@ def main():
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    app.run(debug=True, host = "localhost", port = 8080)
+    main()
---
+++ backend/server.py
@@ -0,0 +1,17 @@
+import os
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from flask import Flask
+
+app = Flask(__name__)
+
+mongo_client = MongoClient(os.getenv("MONGO_CONNECTION"), server_api=ServerApi('1'))
+db = mongo_client['LLM']
+users_collection = db['Values']
+
+@app.route("/")
+def insert():
+    users_collection.insert_one("bruh")
+    
+if __name__ == "__main__":
+    app.run(debug=True, host = "localhost", port = 8080)
