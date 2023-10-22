4f50bfa7a944a61a3b9ef57479d6c6325f91f010
SuhaiBatman <97989770+SuhaiBatman@users.noreply.github.com>
2023-10-22 00:43:57 -0500

    removed unnecessary files

--- Mongo/mongo.py
+++
@@ -1,6 +0,0 @@
-import os
-from pymongo import MongoClient
-
-mongo_client = MongoClient(os.getenv("MONGO_URL"), server_api=ServerApi('1'))
-db = mongo_client['PixEraDB']
-users_collection = db['Users']
