74c81342defad4e6d052fba627c73d11150cbe0f
SuhaiBatman <97989770+SuhaiBatman@users.noreply.github.com>
2023-10-22 03:40:31 -0500

    added dynamic increment

--- backend/bruhbruh.py
+++ backend/bruhbruh.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import screen_brightness_control as sbc
 
 def main():
     # Initialize MediaPipe Hand module
@@ -19,6 +20,8 @@ def main():
     if not cap.isOpened():
         print("Error: Could not open the camera.")
         return
+
+    brightness_increase = 1  # You can adjust this value
 
     while True:
         ret, frame = cap.read()
@@ -45,15 +48,48 @@ def main():
 
                 # Calculate the distance between thumb tip and index finger tip
                 distance = ((thumb_tip.x - index_finger_tip.x) ** 2 + (thumb_tip.y - index_finger_tip.y) ** 2) ** 0.5
+                
+                if(distance < 0.085):
+                    brightness_increase = 1
+                elif(distance < 0.16 and distance >= 0.085):
+                    brightness_increase = 2
+                elif(distance < 0.25 and distance >= 0.16):
+                    brightness_increase = 3
+                elif(distance < 0.35 and distance >= 0.25):
+                    brightness_increase = 4
 
                 # Check if the distance is below a threshold (closed fist)
                 threshold = 0.05  # Adjust this threshold as needed
                 if distance < threshold:
                     cv2.putText(frame, "Closed Fist", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, GREEN, 2)
                     cv2.line(frame, (20, 60), (frame.shape[1] - 20, 60), GREEN, 5)
+                    brightness_levels = sbc.get_brightness()
+                    # Increase screen brightness
+                    if brightness_levels:
+                        current_brightness = brightness_levels[0]
+                    if current_brightness <= 100 and current_brightness > 0:
+                        sbc.set_brightness(current_brightness - brightness_increase)
+                    if current_brightness >= 100 or current_brightness <= 0:
+                        sbc.set_brightness(current_brightness)
+                    print(current_brightness)
+                    
+
+                        
+                    # if sbc.get_brightness() < 100 and sbc.get_brightness() > 0:
+                        # sbc.set_brightness(brightness_original-brightness_increase)
                 else:
                     cv2.putText(frame, "Open Palm", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, RED, 2)
                     cv2.line(frame, (20, 60), (frame.shape[1] - 20, 60), RED, 5)
+                    # Restore original screen brightness
+                    brightness_levels = sbc.get_brightness()
+                    # Increase screen brightness
+                    if brightness_levels:
+                        current_brightness = brightness_levels[0]
+                    if current_brightness < 100 and current_brightness >= 0:
+                        sbc.set_brightness(current_brightness + brightness_increase)
+                    if current_brightness >= 100 or current_brightness <= 0:
+                        sbc.set_brightness(current_brightness)
+                    print(current_brightness)
 
         # Display the video feed with hand landmarks
         cv2.imshow("Hand Gestures", frame)
--- backend/server.py
+++ backend/server.py
@@ -11,7 +11,7 @@ users_collection = db['Values']
 
 @app.route("/")
 def insert():
-    users_collection.insert_one("bruh")
+    users_collection.insert_one({"bruh": "bruh"})
     
 if __name__ == "__main__":
     app.run(debug=True, host = "localhost", port = 8080)
